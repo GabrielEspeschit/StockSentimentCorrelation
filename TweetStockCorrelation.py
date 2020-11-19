2728335dc2a56df0cda45c110ee5a5a44afa4d47
GabrielEspeschit <gabrielespeschit@gmail.com>
2020-11-19 15:14:18 -0300

    Finalizing version 0

--- TweetStockCorrelation.py
+++ TweetStockCorrelation.py
@@ -5,24 +5,38 @@ import nest_asyncio
 import unittest
 import leia
 import nltk.sentiment.vader as vader
-
+import pandas_datareader as data
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import pearsonr
 nest_asyncio.apply()
 
+## CREATE FUNCTION TO SET TIME TO YY-m-d
+## Implement Global timezone diferences
 
 class Tweets:
     '''
     Class responsible for aquirering, processing and given sentiments to tweets, given a username.
     Objects:
-        userName: given by user
-        sinceTime: time to start getting tweets (default to 1 year ago from today)
+        userName (given): given by user
+        sinceTime (given): time to start getting tweets (default to 1 year ago from today)
+        lang (given): language to analise tweets in
         tweetDf: stores the dataframe returned by twint
-        tweetSentiment: stores the tweets and the dates of the tweets as well as each tweet sentiment given by LeIA ou Vader
+        tweetSentiment: stores the tweets and the dates of the tweets as well as each tweet sentiment compound score
+    Methods:
+        __init__: initializes class
+        aquireTweets: aquires tweets using twint
+        getSentiment: gets sentiment using LeIA or Vader (depending on the language)
+        cleanDateTime: cleans datetime and transforms it into usable dtype
+        compileSentiment: compile the mean daily sentiment
+
     '''
 
-    def __init__(self, userName, sinceTime = str((dt.datetime.now() - dt.timedelta(days=365)).date())):
+    def __init__(self, userName, sinceTime = str((dt.datetime.now() - dt.timedelta(days=365)).date()), lang = 'pt'):
         
         self.userName = userName
         self.sinceTime = sinceTime
+        self.lang = lang
 
     def aquireTweets(self):
         
@@ -35,22 +49,97 @@ class Tweets:
         
         self.tweetDf = twint.storage.panda.Tweets_df
         
-        assert self.tweetDf.empty == False, "Did not get any tweets. Please check if the username you typed exists and has an open account"
+        assert self.tweetDf.empty == False, "Did not get any tweets. Please check if the username you typed exists and has an open account or if there are any tweets for the timeframe selected."
     
-    def getSentiment(self, lang = 'pt'):
+    def getSentiment(self):
         
-        self.tweetSentiment = self.tweetDf[['date', 'timezone', 'tweet']]
-        if lang == 'pt':
+        self.tweetSentiment = self.tweetDf[['date', 'tweet']]
+        if self.lang == 'pt':
             s = leia.SentimentIntensityAnalyzer()
             self.tweetSentiment['textPolarity'] = self.tweetSentiment.tweet.apply(lambda frase: s.polarity_scores(frase)['compound'])
-        elif lang == 'en':
+        elif self.lang == 'en':
             a = vader.SentimentIntensityAnalyzer()
             self.tweetSentiment['textPolarity'] = self.tweetSentiment.tweet.apply(lambda frase: a.polarity_scores(frase)['compound'])
         else:
             raise Exception('Please type \'pt\' for Portuguese or \'en\' for English')
 
+        del self.tweetSentiment['tweet']
 
-## CREATE FUNCTION TO SET TIME TO YY-m-d
-   
+    def cleanDateTime(self):
+        self.tweetSentiment['date'] = pd.to_datetime(self.tweetSentiment['date'])
+        self.tweetSentiment['onlyDate'] = [d.date() for d in self.tweetSentiment.date]
+        self.tweetSentiment['onlyTime'] = [d.time() for d in self.tweetSentiment.date]
+
+    def compileSentiment(self):
+        self.cleanDateTime()
+        self.tweetSentiment = self.tweetSentiment.groupby(['onlyDate']).mean()
 
 
+class Stocks():
+    def __init__(self, ticker, startDate = str((dt.datetime.now() - dt.timedelta(days=365)).date())):
+        self.ticker = ticker
+        self.endDate = str(dt.datetime.now().date())
+        self.startDate = startDate
+        
+    def getPrices(self):    
+        self.prices = data.DataReader(self.ticker, 'yahoo', self.startDate, self.endDate)
+
+    def getDailyVar(self):
+        self.prices['var'] = (self.prices.Close - self.prices.Close.shift(1))/(self.prices.Close.shift(1))*100
+
+
+class TweetAndStockData():
+
+    def __init__(self, ticker, username, startDate = str((dt.datetime.now() - dt.timedelta(days=365)).date()), lang = 'pt'):
+        
+        self.username = username
+        self.ticker = ticker
+        
+        tweets = Tweets(userName=username, sinceTime=startDate, lang=lang)
+        tweets.aquireTweets()
+        tweets.getSentiment()
+        tweets.compileSentiment()
+        self.tweetStockData = tweets.tweetSentiment
+
+        stocks = Stocks(ticker=ticker, startDate=startDate)
+        stocks.getPrices()
+        stocks.getDailyVar()
+        self.tweetStockData['change'] = stocks.prices['var']
+        self.tweetStockData.dropna(inplace=True)
+        self.tweetStockData['varNorm'] = (stocks.prices['var']-stocks.prices['var'].min())/(stocks.prices['var'].max()-stocks.prices['var'].min())
+        self.tweetStockData['polNorm'] = (tweets.tweetSentiment['textPolarity']-tweets.tweetSentiment['textPolarity'].min())/(tweets.tweetSentiment['textPolarity'].max()-tweets.tweetSentiment['textPolarity'].min())
+        
+
+    def avaragingNaN(self):
+        pass
+    
+    def plotCorrelation(self, pearson = True):
+        if pearson:
+            data1 = self.tweetStockData.change.to_numpy()
+            data2 = self.tweetStockData.textPolarity.to_numpy()
+            corr, _ = pearsonr(data1, data2)
+            print('Pearsons correlation: %.3f' % corr)
+            cor = self.tweetStockData.change.rolling(3).corr(self.tweetStockData.textPolarity)
+            plt.plot(cor)
+            plt.show()
+
+
+        
+        else:
+            plt.plot(self.tweetStockData[['varNorm', 'polNorm']])
+            plt.title('Variação do Índice Ibovespa e sentimento do twitter analisadas (valores normalizados)', fontsize = 16)
+            plt.xlabel('Dia', fontsize=12)
+            plt.ylabel('Valor', fontsize=12)
+            plt.legend(['Variação do Ibovespa', f'Sentimento do twitter do @{self.username}'], loc='lower right')
+            plt.show()
+
+        
+
+
+if __name__ == '__main__':
+    ticker = '^BVSP'
+    username = 'folha'
+    lang = 'pt'
+    startDate = '2020-10-01'
+    test = TweetAndStockData(ticker=ticker, username=username, startDate= startDate, lang=lang)
+    test.plotCorrelation(pearson=True)
--- TweetStocktest.py
+++ TweetStocktest.py
@@ -1,16 +1,20 @@
 import unittest
 from TweetStockCorrelation import Tweets
+from TweetStockCorrelation import Stocks
+import datetime as dt
+import pandas.api.types as ptypes
 
 class testTweetClass(unittest.TestCase):
     
     def testNameDate(self):
         name = 'realDonaldTrump'
         date = '2020-04-04'
+        lang = 'pt'
         test = Tweets(userName = name, sinceTime = date)
         
         self.assertEqual(test.userName, name)
         self.assertEqual(test.sinceTime, date)
-
+        self.assertEqual(test.lang, lang)
 
     def testAquireTweets(self):
         date = '2020-11-04'
@@ -30,7 +34,57 @@ class testTweetClass(unittest.TestCase):
             test3.aquireTweets()
 
     def testSentiment(self):
-        test = Tweets(userName='Sxebiel', sinceTime='2020-10-01')
+        date = '2020-11-04'
+        test = Tweets(userName='Sxebiel', sinceTime = date, lang='pt')
         test.aquireTweets()
         test.getSentiment()
         self.assertFalse(test.tweetSentiment.empty)
+
+        test2 = Tweets(userName = 'Sxebiel', sinceTime = date, lang='en')
+        test2.aquireTweets()
+        test2.getSentiment()
+        self.assertFalse(test2.tweetSentiment.empty)
+
+        # This is a failiure test, so it should pop an exeption (because 'es' is not a supported language)
+        with self.assertRaises(Exception):
+            test3 = Tweets(userName = 'Sxebiel', sinceTime = date, lang='es')
+            test3.aquireTweets()
+            test3.getSentiment()
+    
+    def testDateTimeCleanup(self):
+            
+        date = '2020-11-04'
+        test = Tweets(userName='Sxebiel', sinceTime = date, lang='pt')
+        test.aquireTweets()
+        test.getSentiment()
+        test.cleanDateTime()
+        self.assertTrue(ptypes.is_datetime64_any_dtype(test.tweetSentiment.date))
+
+    def testTweetSentimentDF(self):
+
+        date = '2020-11-04'
+        test = Tweets(userName='Sxebiel', sinceTime = date, lang='pt')
+        test.aquireTweets()
+        test.getSentiment()
+        test.compileSentiment()
+        self.assertEqual(len(test.tweetSentiment.columns), 1)
+        self.assertEqual(len(test.tweetSentiment), 9)
+
+class testStockClass(unittest.TestCase):
+    
+    def testInit(self):
+        ticker = '^BVSP'
+        startDate = '2020-04-04'
+        endDate = str(dt.datetime.now().date())
+        test = Stocks(ticker=ticker, dateSince=startDate)
+        self.assertEqual(test.startDate, startDate)
+        self.assertEqual(test.ticker, ticker)
+        self.assertEqual(test.endDate, endDate)
+
+    def testgetPrices(self):
+        ticker = '^BVSP'
+        startDate = '2020-04-04'
+        test = Stocks(ticker=ticker, dateSince=startDate)
+        test.getPrices()
+        self.assertFalse(test.prices.empty)
+        self.assertEqual(len(test.prices.columns), 6)
